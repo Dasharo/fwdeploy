f443b0d83725101a21a9ea42aff6ec417a428d61
Piotr Król <piotr.krol@3mdeb.com>
2021-12-14 02:04:11 +0100

    add support fo cbfstool and coreboot stub modification
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 source container-config
 
-( [ "${1}" == "" ] || [ ! -f "${1}" ] ) && \
- echo "Usage: ./run.sh <image>" && exit 1
+( [ "${1}" == "" ] || [ ! -f "${1}" ] || [ "${2}" == "" ] || [ ! -f "${2}" ] ) && \
+ echo "Usage: ./run.sh <fw_dump_bin> <cb_stub_bin>" && exit 1
 
-docker run -v $(readlink -f ${1}):/home/fwdeploy/fw.bin -t $DOCKER_USER_NAME/$DOCKER_IMAGE_NAME:latest fw.bin
+docker run -v $(readlink -f ${1}):/home/fwdeploy/fw.bin -v $(readlink -f ${2}):/home/fwdeploy/cb.bin -t $DOCKER_USER_NAME/$DOCKER_IMAGE_NAME:latest fw.bin cb.bin
 
--- scripts/extract_image.sh
+++ scripts/extract_image.sh
@@ -3,22 +3,31 @@
 ACM_BIOS_GUID="2D27C618-7DCD-41F5-BB10-21166BE7E143"
 SCH5545_EC_FW="D386BEB8-4B54-4E69-94F5-06091F67E0D3"
 
-image="${1}"
+fw_dump_bin="${1}"
+cb_stub_bin="${2}"
 
-( [ "${image}" == "" ] || [ ! -f "${image}" ] ) && \
- echo "Usage: ./extract_image.sh <image>" && exit 1
+( [ "${fw_dump_bin}" == "" ] || [ ! -f "${fw_dump_bin}" ] \
+	|| [ "${cb_stub_bin}" == "" ] || [ ! -f "${cb_stub_bin}" ]) && \
+       	echo "Usage: ./dell_optiplex_9010_dasharo.sh <fw_dump_bin> <cb_stub_bin>" && exit 1
 
 uefiextract=/home/fwdeploy/UEFITool/UEFIExtract/UEFIExtract
+cbfstool=/home/fwdeploy/coreboot/util/cbfstool/cbfstool
 
-${uefiextract} "${image}" ${ACM_BIOS_GUID} -o acm_bios -m body
+${uefiextract} "${fw_dump_bin}" ${ACM_BIOS_GUID} -o acm_bios -m body
 cp acm_bios/body.bin txt_bios_acm.bin
-${uefiextract} "${image}" ${SCH5545_EC_FW} -o ec -m body
+${uefiextract} "${fw_dump_bin}" ${SCH5545_EC_FW} -o ec -m body
 cp ec/body.bin sch5545_ecfw.bin
 
 # get descriptor, me, gbe
-${uefiextract} "${image}" all
-cp ${image}.dump/0\ Descriptor\ region/body.bin descriptor.bin
-cp ${image}.dump/2\ ME\ region/body.bin me.bin
-cp ${image}.dump/1\ GbE\ region/body.bin gbe.bin
+${uefiextract} "${fw_dump_bin}" all
+cp ${fw_dump_bin}.dump/0\ Descriptor\ region/body.bin descriptor.bin
+cp ${fw_dump_bin}.dump/2\ ME\ region/body.bin me.bin
+cp ${fw_dump_bin}.dump/1\ GbE\ region/body.bin gbe.bin
 
 sha256sum -c /home/fwdeploy/blobs/dell_optiplex_9010.sha256
+
+${cbfstool} ${cb_stub_bin} add -t raw -f txt_bios_acm.bin -n txt_bios_acm.bin -a 0x20000 -r COREBOOT
+${cbfstool} ${cb_stub_bin} add -t raw -f sch5545_ecfw.bin -n sch5545_ecfw.bin  -r COREBOOT
+${cbfstool} ${cb_stub_bin} write -r SI_DESC -f descriptor.bin -u
+${cbfstool} ${cb_stub_bin} write -r SI_ME -f me.bin -u
+${cbfstool} ${cb_stub_bin} write -r SI_GBE -f gbe.bin -u
