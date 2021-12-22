a8c5ddeee5c2902e23864066530fae05a7668b2f
Piotr Król <piotr.krol@3mdeb.com>
2021-12-22 23:24:11 +0100

    run.sh: make script independent
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-source container-config
 
 ( [ "${1}" == "" ] || [ ! -f "${1}" ] || [ "${2}" == "" ] || [ ! -f "${2}" ] ) && \
  echo "Usage: ./run.sh <fw_dump_bin> <cb_stub_bin>" && exit 1
 
-docker run -v $(readlink -f ${1}):/home/fwdeploy/fw.bin -v $(readlink -f ${2}):/home/fwdeploy/cb.bin -t $DOCKER_USER_NAME/$DOCKER_IMAGE_NAME:latest fw.bin cb.bin
+docker run -v $(readlink -f ${1}):/home/fwdeploy/fw.bin -v $(readlink -f ${2}):/home/fwdeploy/cb.bin -t dasharo/fwdeploy:latest fw.bin cb.bin
 
